0e17d53a7dcc81bb16ff08a38b547ef38f5efd06
user-pratik <pratikanand113@gmail.com>
2025-04-11 20:09:35 +0530

    npm start updated

--- solomon/src/App.jsx
+++ solomon/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import SinglePlayerGame from "./pages/single";
 
 // Create a wrapper component for SinglePlayerGame
